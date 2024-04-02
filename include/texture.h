dacee8c573be108d2157076d71387f0e3604086e
Dominik Witczak <vvitczak.dominik@gmail.com>
2024-04-02 23:07:43 +0200

    Add texture wrapper

---
+++ include/texture.h
@@ -0,0 +1,117 @@
+/*
+
+    MIT License
+
+    Copyright (c) 2024 Dominik Witczak
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+#if !defined(TEXTURE_H)
+#define TEXTURE_H
+
+#include "framework.h"
+#include <array>
+
+namespace Framework
+{
+    /* Forward decls */
+    class Texture;
+
+    /* Type defs */
+    typedef std::unique_ptr<Texture> TextureUniquePtr;
+
+    /* Enums */
+    enum class TextureFormat
+    {
+        /* Core WebGL 2.0 formats */
+        R10G1B10_A2          = GL_RGB10_A2,
+        R11G11B10_SFLOAT     = GL_R11F_G11F_B10F,
+        R16_SFLOAT           = GL_R16F,
+        R16G16_SFLOAT        = GL_RG16F,
+        R16G16B16_SFLOAT     = GL_RGB16F,
+        R16G16B16A16_SFLOAT  = GL_RGBA16F,
+        R32_SFLOAT           = GL_R32F,
+        R32G32_SFLOAT        = GL_RG32F,
+        R32G32B32_SFLOAT     = GL_RGB32F,
+        R32G32B32A32_SFLOAT  = GL_RGBA32F,
+        R4G4B4A4_UNORM       = GL_RGBA4,
+        R5G5B5_A1            = GL_RGB5_A1,
+        R5G6B5_UNORM         = GL_RGB565,
+        R8_UINT              = GL_R8UI,
+        R8_UNORM             = GL_R8,
+        R8G8_UINT            = GL_RG8UI,
+        R8G8_UNORM           = GL_RG8,
+        R8G8B8_UINT          = GL_RGB8UI,
+        R8G8B8_UNORM         = GL_RGB8,
+        R8G8B8A8_UINT        = GL_RGBA8UI,
+        R9G9B9E5_SFLOAT      = GL_RGB9_E5,
+        SR8G8B8_UNORM        = GL_SRGB8,
+        SR8G8B8_ALPHA8_UNORM = GL_SRGB8_ALPHA8,
+
+        UNKNOWN
+    };
+
+    class Texture
+    {
+    public:
+        /* Public functions */
+        static TextureUniquePtr create_immutable_2d  (const bool&                    in_single_mip,
+                                                      const TextureFormat&           in_format,
+                                                      const std::array<uint32_t, 2>& in_extents,
+                                                      const uint32_t&                in_n_layers);
+        static TextureUniquePtr create_immutable_3d  (const bool&                    in_single_mip,
+                                                      const TextureFormat&           in_format,
+                                                      const std::array<uint32_t, 3>& in_extents);
+        static TextureUniquePtr create_immutable_cube(const bool&                    in_single_mip,
+                                                      const TextureFormat&           in_format,
+                                                      const uint32_t&                in_extents);
+
+        ~Texture();
+
+        GLuint get_id();
+
+    private:
+        /* Private type defs */
+        enum class TextureType : uint8_t
+        {
+            _2D,
+            _3D,
+            CUBE,
+
+            UNKNOWN
+        };
+
+        /* Private functions */
+        Texture(const TextureType&             in_type,
+                const std::array<uint32_t, 3>& in_extents,
+                const TextureFormat&           in_format);
+
+        bool init(const bool& in_mipped);
+
+        /* Private variables */
+        const std::array<uint32_t, 3> m_extents;
+        const TextureFormat           m_format;
+        const TextureType             m_type;
+
+        GLuint                                m_id;
+        std::vector<std::array<uint32_t, 3> > m_mip_size_vec;
+    };
+}
+#endif /* TEXTURE_H */
---
+++ src/texture.cpp
@@ -0,0 +1,227 @@
+/*
+
+    MIT License
+
+    Copyright (c) 2024 Dominik Witczak
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+#include "texture.h"
+#include <algorithm>
+
+Framework::Texture::Texture(const TextureType&             in_type,
+                            const std::array<uint32_t, 3>& in_extents,
+                            const TextureFormat&           in_format)
+    :m_extents(in_extents),
+     m_format (in_format),
+     m_id     (0),
+     m_type   (in_type)
+{
+    /* Stub */
+}
+
+Framework::Texture::~Texture()
+{
+    if (m_id != 0)
+    {
+        glDeleteTextures(1,
+                        &m_id);
+
+        m_id = 0;
+    }
+}
+
+Framework::TextureUniquePtr Framework::Texture::create_immutable_2d(const bool&                    in_single_mip,
+                                                                    const TextureFormat&           in_format,
+                                                                    const std::array<uint32_t, 2>& in_extents,
+                                                                    const uint32_t&                in_n_layers)
+{
+    TextureUniquePtr result_ptr(
+        new Texture(TextureType::_2D,
+                    {in_extents.at(0), in_extents.at(1), 1},
+                    in_format)
+    );
+
+    if (!result_ptr->init(!in_single_mip) )
+    {
+        Framework::report_error("Texture initialization failed.");
+
+        result_ptr.reset();
+    }
+
+    return result_ptr;
+}
+
+Framework::TextureUniquePtr Framework::Texture::create_immutable_3d(const bool&                    in_single_mip,
+                                                                    const TextureFormat&           in_format,
+                                                                    const std::array<uint32_t, 3>& in_extents)
+{
+    TextureUniquePtr result_ptr(
+        new Texture(TextureType::_3D,
+                    in_extents,
+                    in_format)
+    );
+
+    if (!result_ptr->init(!in_single_mip) )
+    {
+        Framework::report_error("Texture initialization failed.");
+
+        result_ptr.reset();
+    }
+
+    return result_ptr;
+}
+
+Framework::TextureUniquePtr Framework::Texture::create_immutable_cube(const bool&          in_single_mip,
+                                                                      const TextureFormat& in_format,
+                                                                      const uint32_t&      in_extents)
+{
+    TextureUniquePtr result_ptr(
+        new Texture(TextureType::CUBE,
+                    {in_extents, in_extents, 6},
+                    in_format)
+    );
+
+    if (!result_ptr->init(!in_single_mip) )
+    {
+        Framework::report_error("Texture initialization failed.");
+
+        result_ptr.reset();
+    }
+
+    return result_ptr;
+}
+
+GLuint Framework::Texture::get_id()
+{
+    if (m_id == 0)
+    {
+        Framework::report_error("Texture ID is 0.");
+    }
+
+    return m_id;
+}
+
+bool Framework::Texture::init(const bool& in_mipped)
+{
+    uint32_t n_mips = 0;
+    bool     result = false;
+
+    /* Allocate an ID */
+    glGenTextures(1,
+                 &m_id);
+
+    if (m_id == 0)
+    {
+        Framework::report_error("glGenTextures() returned a zeroed out ID.");
+
+        goto end;
+    }
+
+    /* Determine how many mips we're going to need. */
+    while (m_mip_size_vec.size() == 0                                ||
+           m_mip_size_vec.back() != std::array<uint32_t, 3>{1, 1, 1})
+    {
+        std::array<uint32_t, 3> new_mip_size;
+
+        new_mip_size =
+        {
+            std::max(1u, m_mip_size_vec.back().at(0) / 2),
+            std::max(1u, m_mip_size_vec.back().at(1) / 2),
+            std::max(1u, m_mip_size_vec.back().at(2) / 2),
+        };
+
+        m_mip_size_vec.emplace_back(new_mip_size);
+    }
+
+    n_mips = static_cast<uint32_t>(m_mip_size_vec.size() );
+
+    /* Set up immutable storage */
+    switch (m_type)
+    {
+        case TextureType::_2D:
+        {
+            if (m_extents.at(2) == 1)
+            {
+                glBindTexture(GL_TEXTURE_2D,
+                              m_id);
+
+                glTexStorage2D(GL_TEXTURE_2D,
+                               n_mips,
+                               static_cast<GLenum>(m_format),
+                               m_extents.at(0),
+                               m_extents.at(1) );
+            }
+            else
+            {
+                glBindTexture(GL_TEXTURE_2D_ARRAY,
+                              m_id);
+
+                glTexStorage3D(GL_TEXTURE_2D_ARRAY,
+                               n_mips,
+                               static_cast<GLenum>(m_format),
+                               m_extents.at(0),
+                               m_extents.at(1),
+                               m_extents.at(2) );
+            }
+            break;
+        }
+
+        case TextureType::_3D:
+        {
+            glBindTexture(GL_TEXTURE_3D,
+                          m_id);
+
+            glTexStorage3D(GL_TEXTURE_3D,
+                           n_mips,
+                           static_cast<GLenum>(m_format),
+                           m_extents.at(0),
+                           m_extents.at(1),
+                           m_extents.at(2) );
+
+            break;
+        }
+
+        case TextureType::CUBE:
+        {
+            glBindTexture(GL_TEXTURE_CUBE_MAP,
+                          m_id);
+
+            glTexStorage2D(GL_TEXTURE_CUBE_MAP,
+                           n_mips,
+                           static_cast<GLenum>(m_format),
+                           m_extents.at(0),
+                           m_extents.at(1) );
+
+            break;
+        }
+
+        default:
+        {
+            Framework::report_error("Invalid texture type requested.");
+
+            goto end;
+        }
+    }
+
+    result = true;
+end:
+    return result;
+}
